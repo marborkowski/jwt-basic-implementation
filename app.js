7583b27dde4af57ef7518c19b912199931af29fb
Marcin Borkowski <marcin.borkowski@rb.com>
2018-04-04 12:14:07 +0200

    require on top

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const config = require('config');
+const controllers = require('./controllers');
 
 const logger = require('./services/logger');
 
 app.set('port', process.env.PORT || config.get('port'));
 app.use(bodyParser.json());
 
-app.use(require('./controllers'));
+app.use(controllers);
 
 app.listen(
   app.get('port'),
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const user = require('./user');
 
-router.use('/user', require('./user'));
+router.use('/user', user);
 
 module.exports = router;
